9a836c8020dfcce2f191af361821b54379483cfd
Pinucoin <damiannav03@gmail.com>
2022-07-21 14:37:51 +0200

    Default Profile
    adding default profile which defaults to said profile if an active one is not specified in the .properties folder

--- src/main/java/damian/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/damian/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -3,7 +3,7 @@ package damian.springframework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN", "default"}) //defaults to this profile if an active profile is not set in the .properties file
 @Service("i18nService")
 public class I18nEnglishGreetingService implements GreetingService{
 
