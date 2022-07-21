9dab49213f16df2e1ca93eafa7632c97c48eee20
Pinucoin <damiannav03@gmail.com>
2022-07-21 14:13:46 +0200

    Primary Bean testing

--- src/main/java/damian/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/damian/springframework/sfgdi/controllers/MyController.java
@@ -1,11 +1,18 @@
 package damian.springframework.sfgdi.controllers;
 
+import damian.springframework.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello(){
-        System.out.println("Hello!");
-        return "Hi";
+        return greetingService.sayGreeting();
     }
 }
